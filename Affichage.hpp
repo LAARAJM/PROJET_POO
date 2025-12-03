8e236c55392362c7baf371b46e4e0726466ba0ee
Mohamed L <mohamed.laaraj@viacesi.fr>
2025-12-03 11:41:08 +0100

    Correction affichage 2

--- Affichage.hpp
+++ Affichage.hpp
@@ -1,17 +1,26 @@
 #pragma once
-#include "Grille.hpp"
+
+#include <string>
+
+// Forward declaration
+namespace NS_Modele {
+    class Grille;
+}
 
 namespace NS_Vue {
+
+
     class Affichage {
-
     public:
+        virtual ~Affichage() = default;
 
-        virtual void initialiser()=0;
-        virtual void afficher(NS_Modele::Grille* grille, int n)=0;
-        virtual void nettoyer()=0;
-        virtual void afficherMessage(const char* message)=0;
-    
+        virtual void initialiser() = 0;
+
+        virtual void afficher(NS_Modele::Grille* grille, int n) = 0;
+
+        virtual void nettoyer() = 0;
+
+        virtual void afficherMessage(const std::string& message) = 0;
     };
 
-};
-
+}
--- AffichageConsole.hpp
+++ AffichageConsole.hpp
@@ -1,56 +1,32 @@
 #pragma once
 
-#include <iostream>
-#include <fstream>
-#include <filesystem>
+#include <string>
 #include "Affichage.hpp"
-#include "Grille.hpp"
+
+namespace NS_Modele {
+    class Grille;
+}
 
 namespace NS_Vue {
 
-class AffichageConsole : public Affichage {
-private:
-    std::string dossierSortie;
-    int numeroIteration;
+    class AffichageConsole : public Affichage {
+    private:
+        int numeroIteration;
 
-public:
-    AffichageConsole(const std::string& dossier)
-        : dossierSortie(dossier), numeroIteration(0) {}
+    public:
 
-    void initialiser() override {
-        std::filesystem::create_directories(dossierSortie);
-        numeroIteration = 0;
-    }
+        AffichageConsole();
 
-    void afficher(NS_Modele::Grille* grille, int n) override {
-        numeroIteration = n;
 
-        std::cout << "Iteration : " << n << std::endl;
+        void initialiser() override;
 
-        for (int y = 0; y < grille->getHauteur(); ++y) {
-            for (int x = 0; x < grille->getLargeur(); ++x) {
-                std::cout << grille->obtenirCellule(x, y)->getSymbole();
-            }
-            std::cout << std::endl;
-        }
+        void afficher(NS_Modele::Grille* grille, int n) override;
 
-        // Sauvegarde dans un fichier
-        std::ofstream fichier(dossierSortie + "/iter_" + std::to_string(n) + ".txt");
-        for (int y = 0; y < grille->getHauteur(); ++y) {
-            for (int x = 0; x < grille->getLargeur(); ++x) {
-                fichier << grille->obtenirCellule(x, y)->getSymbole();
-            }
-            fichier << "\n";
-        }
-    }
+ 
+        void nettoyer() override;
 
-    void nettoyer() override {
-        std::cout << "Nettoyage console..." << std::endl;
-    }
-
-    void afficherMessage(const char* message) override {
-        std::cout << "[MESSAGE] " << message << std::endl;
-    }
-};
+  
+        void afficherMessage(const std::string& message) override;
+    };
 
 }
