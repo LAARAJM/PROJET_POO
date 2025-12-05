37bce513ed682cc5f237c1120754af85eb5c7cbd
RyanLSM <ryan.mourandee@viacesi.fr>
2025-12-05 10:48:20 +0100

    commentaires 4

--- NS_Controleur/GestionFichier.hpp
+++ NS_Controleur/GestionFichier.hpp
@@ -2,21 +2,24 @@
 
 #include <string>
 
-namespace NS_Modele {
-    class Grille;
+namespace NS_Modele { //Namespace du modèle
+    class Grille; //Déclaration anticipée de la classe Grille
 }
 
-namespace NS_Controleur {
+namespace NS_Controleur { //Namesapce du contrôleur
 
-    class GestionFichier {
+    class GestionFichier { //Déclaration de la classe GestionFichier
     public:
-        static NS_Modele::Grille* chargerGrille(const std::string& cheminFichier);
+        //Méthode statique pour charger une grille 
+        static NS_Modele::Grille* chargerGrille(const std::string& cheminFichier); //Paramètre : chemin du fichier à lire
         
-        static void sauvegarderGrille(const std::string& cheminFichier, 
-                                      NS_Modele::Grille* grille, 
-                                      int iteration);
+        //Méthode statique pour sauvegarder une grille
+        static void sauvegarderGrille(const std::string& cheminFichier, //Paramètre : dossier ou fichier de sortie
+                                      NS_Modele::Grille* grille, //Paramètre : grille à sauvegarder
+                                      int iteration); //Paramètre : numéro de l'itération
         
-        static bool validerFormat(const std::string& cheminFichier);
+        //Méthode statique pour valider un fichier
+        static bool validerFormat(const std::string& cheminFichier); //Paramètre : chemin du fichier à vérifier
     };
 
 }
