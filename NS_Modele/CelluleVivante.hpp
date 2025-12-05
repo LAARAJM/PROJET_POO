1d7fd998119f75055b91cdac4813721bb86f3297
RyanLSM <ryan.mourandee@viacesi.fr>
2025-12-05 13:43:20 +0100

    commentaires 12

--- NS_Modele/CelluleVivante.hpp
+++ NS_Modele/CelluleVivante.hpp
@@ -2,19 +2,19 @@
 
 #include "Cellule.hpp" 
 
-namespace NS_Modele {
-    class CelluleVivante : public Cellule {
+namespace NS_Modele { //Namespace du modèle
+    class CelluleVivante : public Cellule { //Cellule Vivante hérite de la classe abstraite Cellule
     public:
-        CelluleVivante();
-        virtual ~CelluleVivante() = default;
+        CelluleVivante(); //Constructeur de la Cellule vivante
+        virtual ~CelluleVivante() = default; //Destructeur virtuel par défaut
 
-        virtual bool estVivante() override;
+        virtual bool estVivante() override; //Indique que la cellule est vivante
 
-        virtual char getSymbole() override;
+        virtual char getSymbole() override; //Retourne le symbole de la cellule
 
-        virtual bool estObstacle() override;
+        virtual bool estObstacle() override; //Indique si la cellule est un obstacle
 
-        virtual Cellule* clone() override; 
+        virtual Cellule* clone() override; //Retourne une copie polymorphique de la cellule 
 
     };
 }
