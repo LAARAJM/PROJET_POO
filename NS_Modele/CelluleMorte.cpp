3d3d299ee799b30ccd027dd1e02574c7217eeffd
RyanLSM <ryan.mourandee@viacesi.fr>
2025-12-05 11:41:56 +0100

    commentaires 7

--- NS_Modele/CelluleMorte.cpp
+++ NS_Modele/CelluleMorte.cpp
@@ -1,28 +1,28 @@
 #include "CelluleMorte.hpp"
 
-namespace NS_Modele {
+namespace NS_Modele { //Namespace du modèle
 
 
-    CelluleMorte::CelluleMorte() {}
+    CelluleMorte::CelluleMorte() {} //Constructeur d'une cellule morte
 
 
-    bool CelluleMorte::estVivante() {
-        return false;
+    bool CelluleMorte::estVivante() { //Méthode indiquant si la cellule est vivante
+        return false; //Une cellule morte n'est jamais vivante
     }
 
 
-    char CelluleMorte::getSymbole() {
-        return '0'; 
+    char CelluleMorte::getSymbole() { //Retourne le symbole associé à la cellule
+        return '0'; //'0' représente une cellule morte
     }
 
 
-    bool CelluleMorte::estObstacle() {
-        return false;
+    bool CelluleMorte::estObstacle() { //Indique si la cellule est un obstacle
+        return false; //Une cellule morte n'est pas un obstacle
     }
 
 
-    Cellule* CelluleMorte::clone() {
-        return new CelluleMorte(*this);
+    Cellule* CelluleMorte::clone() { //Crée une copie polymorphique de la cellule 
+        return new CelluleMorte(*this); //Retourne une nouvelle instance copiée
     }
     
 }
