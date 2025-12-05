7978cf0c0475269e81117de1e0bb331a07b1f913
RyanLSM <ryan.mourandee@viacesi.fr>
2025-12-05 14:10:28 +0100

    commentaires 15

--- NS_Modele/Position.cpp
+++ NS_Modele/Position.cpp
@@ -1,8 +1,8 @@
 #include "Position.hpp"
 
-namespace NS_Modele {
+namespace NS_Modele { //Namespace du modèle
 
-    bool Position::estValide(int largeurMax, int hauteurMax) {
-        return x >= 0 && x < largeurMax && y >= 0 && y < hauteurMax;
+    bool Position::estValide(int largeurMax, int hauteurMax) { //Vérifie si la position est dans les limites
+        return x >= 0 && x < largeurMax && y >= 0 && y < hauteurMax; //Retourne true si x et y sont dans la grille, sinon false
     }
 }
