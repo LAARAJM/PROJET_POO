fe434250b39412e42f25ee22444b5539d3ab1471
RyanLSM <ryan.mourandee@viacesi.fr>
2025-12-05 14:18:36 +0100

    commentaires 17

--- NS_Modele/Regle.hpp
+++ NS_Modele/Regle.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include "Cellule.hpp" 
 
-namespace NS_Modele {
+namespace NS_Modele { //Namespace du modèle
 
-    class Regle {
+    class Regle { //Déclaration de la classe abstraite Regle
     public:
 
-        virtual Cellule* obtenirNouvelEtat(Cellule* celluleActuelle, int nbVoisinsVivants) = 0;
+        //Calcule le nouvel état d'une cellule
+        virtual Cellule* obtenirNouvelEtat(Cellule* celluleActuelle, int nbVoisinsVivants) = 0; //Cellule à analyser et nombre de voisins vivants
 
-        virtual ~Regle() = default;
+        virtual ~Regle() = default; //Destructeur virtuel par défaut
         
     };
 }
