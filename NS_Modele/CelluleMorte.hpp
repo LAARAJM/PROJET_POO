cf516eac2f3bafb7f5dbd6128d86c55a7d208115
RyanLSM <ryan.mourandee@viacesi.fr>
2025-12-05 11:46:17 +0100

    commentaires 8

--- NS_Modele/CelluleMorte.hpp
+++ NS_Modele/CelluleMorte.hpp
@@ -2,22 +2,22 @@
 
 #include "Cellule.hpp" 
 
-namespace NS_Modele {
+namespace NS_Modele { //Namespace du modèle
 
-    class CelluleMorte : public Cellule {
+    class CelluleMorte : public Cellule { //Cellule Morte hérite de la classe abstraite Cellule
     public:
 
-        CelluleMorte();
+        CelluleMorte(); //Constructeur de la cellule morte
 
-        virtual ~CelluleMorte() = default;
+        virtual ~CelluleMorte() = default; //Destructeur virtuel par défaut
 
-        virtual bool estVivante() override;
+        virtual bool estVivante() override; //Indique si la cellule est vivante
 
-        virtual char getSymbole() override;
+        virtual char getSymbole() override; //Retourne le symbole représentant la cellule
 
-        virtual bool estObstacle() override;
+        virtual bool estObstacle() override; //Indique si c'est un obstacle 
 
-        virtual Cellule* clone() override; 
+        virtual Cellule* clone() override; //Retourne une copie polymorphique de l'objet
         
     };
 }
